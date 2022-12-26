0f33b549a157f1a6ac7cc095396cd17b2a4f7958
Marcio Miqueias G Inacio <mmgi.marcio@gmail.com>
2022-12-26 10:22:28 -0300

    Finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+//Modules
+import { PagesModule } from './pages/pages.module';
+import { RouterModule } from '@angular/router';
+
+// Components
 
 @NgModule({
   declarations: [
@@ -10,7 +17,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
+    HttpClientModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
